c4529c41d0820c5bf116a1e84a58c2924777926e
Farah-Tarek <134189708+Farah-Tarek@users.noreply.github.com>
2024-05-10 15:07:05 +0300

    Update database.js

---
+++ Backend/src/database/database.js
@@ -0,0 +1,21 @@
+const AWS = require('aws-sdk');
+require('dotenv').config();
+
+AWS.config.update({
+    region: "us-west-1", // Corrected region name
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+const db = new AWS.DynamoDB.DocumentClient();
+
+const Table_users = 'users';
+const Table_event = 'Event';
+const Table_admin = 'Admin';
+
+module.exports = {
+    db,
+    Table_users,
+    Table_event,
+    Table_admin
+};
