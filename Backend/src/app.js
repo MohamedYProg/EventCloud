361765859502f7234fee6655e8b3653a4336e9cc
Farah-Tarek <134189708+Farah-Tarek@users.noreply.github.com>
2024-05-10 18:36:38 +0300

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => res.send('Hello world!'))
+// Parse JSON bodies for POST requests
+
+// Other middleware and routes setup...
+
+app.use(bodyParser.json());
+
+const userRouter = require('./routes/userRoute.js');
+const eventsRoute = require('./routes/eventRoute.js');
+
+
+//app.get('/', (req, res) => res.send('Hello world!'))
+app.use('/api', userRouter);
+app.use('/api', eventsRoute);
+
+
 
 module.exports = app;
