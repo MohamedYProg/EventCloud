000b65c75ddb93d47f73ef63c0ee4a9d3ab1f314
Farah-Tarek <134189708+Farah-Tarek@users.noreply.github.com>
2024-05-14 17:42:51 +0300

    Update userRoute.js

--- Backend/src/routes/userRoute.js
+++ Backend/src/routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { login } = require('../controllers/userController.js');
+const { login, register,user_create_event } = require('../controllers/userController.js');
 
 // Route to handle user login
 router.post('/login', async (req, res) => {
@@ -18,6 +18,47 @@ router.post('/login', async (req, res) => {
         res.status(200).json({ message: 'Login successful', user });
     } catch (error) {
         console.error("Error logging in:", error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+// Route to handle user registration
+router.post('/register', async (req, res) => {
+    const { name, dob, email, password, ImageProfile } = req.body;
+
+    try {
+        if (!name || !dob || !email || !password || !ImageProfile) {
+            return res.status(400).json({ error: 'All fields are required for registration' });
+        }
+
+        // Call the register function from the controller
+        const result = await register(name, dob, email, password, ImageProfile);
+        
+        // If registration is successful, you can return a success message
+        res.status(201).json({ message: 'Registration successful' });
+    } catch (error) {
+        console.error("Error registering user:", error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+
+router.post('/create_event', async (req, res) => {
+    const { name, date, Capacity, Location, BookedPlaces, Owner, Category, Image, Duration, Description } = req.body;
+
+    try {
+        // Check if all required fields are provided
+        if (!name || !date || !Capacity || !Location || !Owner || !Category || !Image || !Duration || !Description) {
+            return res.status(400).json({ error: 'All fields are required for creating an event' });
+        }
+
+        // Call the user_create_event function from the controller
+        const result = await user_create_event(name, date, Capacity, Location, BookedPlaces, Owner, Category, Image, Duration, Description);
+        
+        // If event creation is successful, you can return a success message
+        res.status(201).json({ message: 'Event created successfully' });
+    } catch (error) {
+        console.error("Error creating event:", error);
         res.status(500).json({ error: 'Internal server error' });
     }
 });
