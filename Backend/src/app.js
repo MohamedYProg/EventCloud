b4bb355ac2b954869389327d5ff7ac48450e9399
MohamedYProg <mohamedelwa7a@gmail.com>
2024-05-16 04:45:56 +0300

    uncommented cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,12 +13,12 @@ const eventsRoute = require('./routes/eventRoute.js');
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-// app.use(cors({
-//     origin: process.env.ORIGIN,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     credentials: true
-// }))
-app.use(cors());
+app.use(cors({
+    origin: process.env.ORIGIN,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}))
+
 
 //app.use('/api/v1', authRouter)
 //app.use(authenticationMiddleware)
