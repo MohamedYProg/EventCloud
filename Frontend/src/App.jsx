e585acb85b08bab5f582e18c56ab5654cfda36d6
ahossam2703 <ahossam03@gmail.com>
2024-05-17 06:50:00 +0300

    hmmm

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './HomePage';
-// import EventsPage from './EventsPage';
+import EventsPage from './EventsPage';
 import Login from './Login';
 import Signup from './Signup';
 import AboutPage from './AboutPage';
 import ContactUsPage from './ContactUsPage';
-import EventForm from './EventForm';
-import UpdateEventForm from './UpdateEventForm';
 
 // Layout component
 const Layout = ({ children }) => {
@@ -28,12 +26,12 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/events" element={<EventsPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/events" element={<Layout><EventsPage /></Layout>} />
           <Route path="/about" element={<Layout><AboutPage /></Layout>} />
           <Route path="/contact" element={<Layout><ContactUsPage /></Layout>} />
-          <Route path="/events/create" element={<EventForm />} />
-          <Route path="/events/update/:id" element={<UpdateEventForm />} />
         </Routes>
       </div>
     </Router>
